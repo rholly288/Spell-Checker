e85a018bcda241f24b2ae8ad4e77b463add1a145
rholly288 <107590876+rholly288@users.noreply.github.com>
2022-06-20 15:38:07 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # Created By: Rachel Holly
 # Created Date: 6/15/2022
 # Last Edited: 6/18/2022
-# Version: 1.01
+# Version: 1.02
 # ----------------------------------------------------------------------------------------------------------------------
 """ This is the main.py module for the Spell Checker. This module prompts user input for a file name and works alongside
 the searching class and spelling module to return the misspelled words in the text file and the list of possible
@@ -22,6 +22,11 @@ def run_search(file_name):
     print(file.__str__())
 
 
+def edits(filename, incorrect, correction):
+    file = Searching(file_name)
+    file.edit_file(incorrect, correction)
+
+
 print('Welcome to the Spell Checker Program! This program will find the incorrectly spelled words in a text file and '
       'make suggestions for corrections.')
 
@@ -29,6 +34,18 @@ keep_running = True
 while keep_running:
     file_name = input('Please enter the file name: ')
     run_search(file_name)
+    edit = input('Would you like to replace any of the above misspelled words in the text file? (Y/N): ')
+    multiple_edits = True
+    while multiple_edits:
+        if edit == 'Y' or edit == 'y':
+            incorrect = input('Which misspelled word would you like to replace: ')
+            correction = input('Which word would you like to replace it with: ')
+            edits(file_name, incorrect, correction)
+            print('The correction has been made to the file.')
+        edit = input('Is there another word you\'d like to replace? (Y/N): ')
+        if edit == 'N' or edit == 'n':
+            multiple_edits = False
+
     cont = input('Would you like to correct another file? (Y/N): ')
     if cont == 'N' or cont == 'n':
         keep_running = False
